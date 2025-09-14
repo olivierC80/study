9429e3860d4ab90711647570935ee4b5167484e2
olivierC80 <157806971+olivierC80@users.noreply.github.com>
2025-09-14 13:13:52 +0200

    Update library.js

--- library.js
+++ library.js
@@ -4,24 +4,175 @@ window.LIBRARY = {
       "title": "Ellipses — Maths MPSI",
       "storagePrefix": "ellipses-mpsi",
       "chapters": [
-        {
-          "id": "logique",
-          "name": "Logique et raisonnement",
-          "count": 35,
-          "prefix": "1."
-        },
-        {
-          "id": "ensembles",
-          "name": "Ensembles & applications",
-          "count": 30,
-          "prefix": "2."
-        },
-        {
-          "id": "complexes",
-          "name": "Nombres complexes",
-          "count": 34,
-          "prefix": "4."
-        }
+    {
+      "id": "logique",
+      "name": "Logique et raisonnement",
+      "count": 20,
+      "prefix": "1."
+    },
+    {
+      "id": "ensembles",
+      "name": "Ensembles et applications",
+      "count": 21,
+      "prefix": "2."
+    },
+    {
+      "id": "calcul_algebrique_trigonometrique",
+      "name": "Calcul algébrique et trigonométrique",
+      "count": 22,
+      "prefix": "3."
+    },
+    {
+      "id": "complexes",
+      "name": "Nombres complexes",
+      "count": 23,
+      "prefix": "4."
+    },
+    {
+      "id": "systemes_lineaires",
+      "name": "Systèmes linéaires",
+      "count": 10,
+      "prefix": "5."
+    },
+    {
+      "id": "fonctions",
+      "name": "Fonctions",
+      "count": 19,
+      "prefix": "6."
+    },
+    {
+      "id": "fonctions_usuelles",
+      "name": "Fonctions usuelles",
+      "count": 19,
+      "prefix": "7."
+    },
+    {
+      "id": "primitives_equations_diff",
+      "name": "Primitives, équations différentielles",
+      "count": 19,
+      "prefix": "8."
+    },
+    {
+      "id": "suites",
+      "name": "Suites numériques",
+      "count": 21,
+      "prefix": "9."
+    },
+    {
+      "id": "limite_continuite",
+      "name": "Limite et continuité",
+      "count": 17,
+      "prefix": "10."
+    },
+    {
+      "id": "derivabilite",
+      "name": "Dérivabilité",
+      "count": 21,
+      "prefix": "11."
+    },
+    {
+      "id": "arithmetique",
+      "name": "Arithmétique",
+      "count": 22,
+      "prefix": "12."
+    },
+    {
+      "id": "structures_algebriques",
+      "name": "Structures algébriques",
+      "count": 17,
+      "prefix": "13."
+    },
+    {
+      "id": "calcul_matriciel",
+      "name": "Calcul matriciel",
+      "count": 18,
+      "prefix": "14."
+    },
+    {
+      "id": "polynomes_fractions",
+      "name": "Polynômes et fractions rationnelles",
+      "count": 20,
+      "prefix": "15."
+    },
+    {
+      "id": "analyse_asymptotique",
+      "name": "Analyse asymptotique",
+      "count": 19,
+      "prefix": "16."
+    },
+    {
+      "id": "espaces_vectoriels_applications",
+      "name": "Espaces vectoriels, applications linéaires",
+      "count": 16,
+      "prefix": "17."
+    },
+    {
+      "id": "espaces_vectoriels_dimension_finie",
+      "name": "Espaces vectoriels de dimension finie",
+      "count": 16,
+      "prefix": "18."
+    },
+    {
+      "id": "matrices_applications_lineaires",
+      "name": "Matrices et applications linéaires",
+      "count": 19,
+      "prefix": "19."
+    },
+    {
+      "id": "determinants",
+      "name": "Déterminants",
+      "count": 19,
+      "prefix": "20."
+    },
+    {
+      "id": "espaces_prehilbertiens",
+      "name": "Espaces préhilbertiens réels",
+      "count": 15,
+      "prefix": "21."
+    },
+    {
+      "id": "integration",
+      "name": "Intégration",
+      "count": 16,
+      "prefix": "22."
+    },
+    {
+      "id": "series_numeriques",
+      "name": "Séries numériques",
+      "count": 18,
+      "prefix": "23."
+    },
+    {
+      "id": "familles_sommables",
+      "name": "Familles sommables",
+      "count": 18,
+      "prefix": "24."
+    },
+    {
+      "id": "denombrement",
+      "name": "Dénombrement",
+      "count": 21,
+      "prefix": "25."
+    },
+    {
+      "id": "probabilites_univers_fini",
+      "name": "Probabilités sur un univers fini",
+      "count": 16,
+      "prefix": "26."
+    },
+    {
+      "id": "variables_aleatoires",
+      "name": "Variables aléatoires",
+      "count": 15,
+      "prefix": "27."
+    },
+    {
+      "id": "fonctions_deux_variables",
+      "name": "Fonctions de deux variables",
+      "count": 20,
+      "prefix": "28."
+    }
+
       ]
     },
     "physique-mpsi": {
